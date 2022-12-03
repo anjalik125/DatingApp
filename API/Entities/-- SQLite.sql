78c5c75cc113f0512c651ec7272c2d42d63c9170
Anjali <anjalik125@gmail>
2022-12-04 00:14:21 +0530

    git first commit section 2

---
+++ API/Entities/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Anjali");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Shubham");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Rajesh");
